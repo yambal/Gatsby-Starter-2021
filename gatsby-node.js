3ee51ebaf4c352cdabeb4473a8debab7f6e6c446
June YAMAMOTO <june@yambal.net>
2021-09-19 19:23:16 +0900

    gatsby-remark-images gatsby-plugin-sharp gatsby-remark-relative-images

---
+++ gatsby-node.js
@@ -0,0 +1,43 @@
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  console.log(JSON.stringify(result.data, null, 2))
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/Posts.jsx`),
+      context: {
+        slug: node.fields.slug,
+      },
+    })
+  })
+
+}

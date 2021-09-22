460602da99711c74315ff5222ef1bd134a331127
June YAMAMOTO <june@yambal.net>
2021-09-22 22:07:57 +0900

    Revert "test 7"
    This reverts commit d0eb0e6e8652fa6b51cb6ea4b8684fd3d33fa0f8.

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -9,6 +9,9 @@ export type AppTheme = typeof theme
 declare module '@xstyled/system' {
   export interface Theme extends AppTheme {}
 }
+
+/*
 declare module 'styled-components' {
   export interface DefaultTheme extends AppTheme {}
 }
+*/

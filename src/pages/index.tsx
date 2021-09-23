a5db4ee22df64febe4844d46a1e687b084dac136
June YAMAMOTO <june@yambal.net>
2021-09-23 13:20:25 +0900

    Style Provider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,19 @@
 import * as React from "react"
 import { graphql, PageProps } from "gatsby"
 import { Container } from "../components/xBootStyle/Container"
-import { ThemeProvider } from "styled-components"
-import { theme } from "../style/theme"
+import { StyleProvider } from "../providers/StyleProvider"
 
 // markup
 const IndexPage:React.FC<PageProps<GatsbyTypes.IndexPageQuery>> = (props) => {
   const siteTitle = props.data.site?.siteMetadata?.title
 
   return (
-    <ThemeProvider theme={theme} >
+    <StyleProvider>
       <Container>
         <h1>{siteTitle}</h1>
         <h2>Hello</h2>
       </Container>
-    </ThemeProvider>
+    </StyleProvider>
   )
 }
 

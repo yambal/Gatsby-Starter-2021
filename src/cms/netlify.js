c5ddb0ab6fbab6dce2d1151fe6cd2f4d513a9d9b
June YAMAMOTO <june@yambal.net>
2021-10-31 18:27:17 +0900

    test7

--- src/cms/netlify.js
+++ src/cms/netlify.js
@@ -7,7 +7,7 @@ import CMS from 'netlify-cms-app'
  */
 const ArticlePreview = ({ entry, widgetFor }) => (
     <x.article backgroundColor="#FF0000">
-      <h1>entry.getIn(['data', 'title'])</h1>
+      <h1>{entry.getIn(['data', 'title'])}</h1>
       <section dangerouslySetInnerHTML={{ __html: widgetFor('body') }} />
       <pre>{JSON.stringify(entry, null, 2)}</pre>
     </x.article>

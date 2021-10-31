29e4d859ea90c78ff53934feb5fa5caf34f283d4
June YAMAMOTO <june@yambal.net>
2021-10-31 18:21:21 +0900

    test6

--- src/cms/netlify.js
+++ src/cms/netlify.js
@@ -5,10 +5,10 @@ import CMS from 'netlify-cms-app'
  * https://www.stephenlewis.me/notes/custom-netlify-cms-preview-gatsby/
  * @returns 
  */
-const ArticlePreview = ({ entry }) => (
-    <x.article backgroundColor="red">
-      <h1>Custom article preview</h1>
-      <p>This is my new article.</p>
+const ArticlePreview = ({ entry, widgetFor }) => (
+    <x.article backgroundColor="#FF0000">
+      <h1>entry.getIn(['data', 'title'])</h1>
+      <section dangerouslySetInnerHTML={{ __html: widgetFor('body') }} />
       <pre>{JSON.stringify(entry, null, 2)}</pre>
     </x.article>
 )

a178a1fd19d07fa6de69b21e0ca18ee2aec08e57
June YAMAMOTO <june@yambal.net>
2021-10-31 18:07:44 +0900

    test3

--- src/cms/netlify.js
+++ src/cms/netlify.js
@@ -9,16 +9,15 @@ import { theme } from '../style/theme'
  * https://www.stephenlewis.me/notes/custom-netlify-cms-preview-gatsby/
  * @returns 
  */
-const ArticlePreview = () => (
+const ArticlePreview = ({ entry }) => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
     <x.article backgroundColor="red">
       <h1>Custom article preview</h1>
       <p>This is my new article.</p>
+      <pre>{JSON.stringify(entry, null, 2)}</pre>
     </x.article>
   </ThemeProvider>
 )
-
-
 
 CMS.registerPreviewTemplate('index', ArticlePreview)
